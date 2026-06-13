59a1667ca584b54781f5851bca73ba6721e1b225
fluffyboyIvit <1ibartysi1@gmail.com>
2026-06-13 12:38:34 +0400

    update program.java in lesson4_task1

--- lesson4/task1/data/Program.java
+++ lesson4/task1/data/Program.java
@@ -1,14 +1,14 @@
 
-package lesson3.task1;
+package lesson4.task1.data;
 
 import java.util.Iterator;
 
 public class Program {
 
     public static void main(String[] args) {
-        Student vanya = new Student("Ivan", "Ivanov", "Ivanovich", 01);
-        Student sanya = new Student("Alex", "Alexandrov", "Alexandrovich", 02);
-        Student katya =new Student("Ekaterina", "katerinova", "Petrova", 03);
+        Student vanya = new Student("Ivan", "Ivanov", "Ivanovich", 01,01L);
+        Student sanya = new Student("Alex", "Alexandrov", "Alexandrovich", 02,02L);
+        Student katya =new Student("Ekaterina", "katerinova", "Petrova", 03,03L);
 
         StudyGroup sg = new StudyGroup();
         sg.addStudents(vanya);
@@ -17,7 +17,7 @@ public class Program {
         Iterator<Student> iter = new StudyGroupIterator(sg);
         while (iter.hasNext()) {
          Student a =  iter.next();
-         System.out.println(a.getFirstname()+" "+  a.getStud_id());
+         System.out.println( a.getFirstName()+" "+  a.getStudID());
         }
 
          
